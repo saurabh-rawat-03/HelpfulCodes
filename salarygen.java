3449da258d12e3780e119921712ca193b9b95864
saurabh singh rawat <saurabhgmis@gmail.com>
2024-02-08 16:38:46 +0530

    Add Salaray Generator Code

---
+++ salarygen.java
@@ -0,0 +1,69 @@
+/*
+ * input id, name, basic salary
+ * hra 50% of bs
+ * ta 30
+ * da 25
+ * ma 20
+ * tax sal< 5lks == 0
+ *      sal>5 & < 7 = 10%
+ *      sal >7 & <10 =15%
+ *      sal > 10 = 20%
+ * 
+ */
+
+ import java.util.Scanner;
+public class salarygen {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter User Name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter User ID: ");
+        int id = sc.nextInt();
+        
+        System.out.print("Enter User Basic Salary: ");
+        double basicSal = sc.nextFloat();
+        
+
+        double hra, ta, da, ma, tax=0;
+
+        hra = (0.3 * basicSal);
+        ta = basicSal * (0.3);
+        da = basicSal * (0.25);
+        ma = basicSal * (0.2);
+
+        double grossSal = hra + ta + da + ma + basicSal;
+
+
+        if(basicSal <= 500000){
+            tax = 0;
+        }else if(basicSal >500000 && basicSal <= 700000){
+            tax = basicSal * (0.1);
+        }else if(basicSal > 700000 && basicSal <= 1000000){
+            tax = basicSal * (0.15);
+        }else{
+            tax = basicSal * (0.2);
+        }
+
+        grossSal = grossSal - tax;
+        double pf = basicSal * (0.1);
+        grossSal = grossSal - pf;
+
+        System.out.println("------Employee Salary Slip------");
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Basic Salary: " + basicSal);
+        System.out.println("HRA = " + hra);
+        System.out.println("TA = " + ta);
+        System.out.println("DA = " + da);
+        System.out.println("MA = " + ma);
+        System.out.println("PF = "+pf);
+        System.out.println("TAX = " + tax); 
+
+        System.out.println("\n Gross Salary = " + grossSal);
+
+    }
+    
+}
--- try.java
+++ try.java
@@ -1,5 +1,26 @@
 class java{
     public static void main(String[] args) {
-        System.out.println("works");
+        String name = "shuBHam sHarMA";
+        String prc="";
+        String s = " ";
+
+        name = name.toLowerCase();
+
+        int len = name.length();
+
+        for(int i=0; i<len; i++){
+            // char c = name.charAt(i);
+            if(i == 0){
+                prc = (prc + name.charAt(i)).toUpperCase();
+            }else if((String.valueOf(name.charAt(i))).equals(s)){
+                prc = (prc + " " +String.valueOf(name.charAt(i+1))).toUpperCase();
+                i++;
+            }else{
+                prc = prc + name.charAt(i);
+            }
+        }
+
+        System.out.println(prc);
+
     }
-}
+}           
